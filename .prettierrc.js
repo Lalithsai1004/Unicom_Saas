1c4012bb7b6a0f3909a770df9ce12859db6acbae
Lalith <12211019.lalith.cse2022@gmail.com>
2025-08-06 20:03:46 +0530

    workflow updation- removed unused runs

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,5 @@ module.exports = {
     trailingComma: 'es5', // Trailing commas only where valid in ES5 (objects, arrays, not function arguments).
     singleQuote: true,
     printWidth: 120,
-    tabWidth: 4,
+    tabWidth: 5,
 };
